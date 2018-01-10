04d2ee07a26614ccb813ddc00179050b438b3bdf
Unknown <cpd015@gmail.com>
2018-01-10 11:09:57 -0600

    updated message

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	
 	public static void main (String[]args){
 		
-		System.out.println("hello world");  //testing the git interface
+		System.out.println("hello GitKraken");  //testing the git interface
 		
 	}//end main
 	
