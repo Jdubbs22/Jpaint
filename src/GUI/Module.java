23f90e840ac3a354e62e6cf2bfa4ee1afee6ad98
Unknown <cpd015@gmail.com>
2018-01-11 09:35:25 -0600

    added shapes, no content

---
+++ src/GUI/Module.java
@@ -0,0 +1,16 @@
+package GUI;
+
+public abstract class Module {
+	
+		  
+		  // Method descriptor #4 (Lview/EventName;Lview/interfaces/IEventCallback;)V
+		  public abstract void addEvent(view.EventName arg0, view.interfaces.IEventCallback arg1);
+		  
+		  // Method descriptor #6 (Lview/interfaces/IDialogChoice;)Ljava/lang/Object;
+		  // Signature: <T:Ljava/lang/Object;>(Lview/interfaces/IDialogChoice;)TT;
+		  public abstract java.lang.Object getDialogResponse(view.interfaces.IDialogChoice arg0);
+		
+	
+	
+	
+}
